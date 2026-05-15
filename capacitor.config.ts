6e8e29775e05b7566174e9f5ffa5a015597fbe4b
realtvop <realtvop@realtvop.top>
2026-05-15 11:28:23 +0800

    feat: init capacitor with ios dest

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'github.realtvop.aonsoku',
+  appName: 'Aonsoku',
+  webDir: 'dist'
+};
+
+export default config;
