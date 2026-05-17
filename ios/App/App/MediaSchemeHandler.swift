7eef903c1e880646ede6496b1a66da96c994e25c
realtvop <realtvop@realtvop.top>
2026-05-18 01:33:35 +0800

    fix(ios): guard media scheme task lifecycle

--- ios/App/App/MediaSchemeHandler.swift
+++ ios/App/App/MediaSchemeHandler.swift
@@ -9,9 +9,8 @@ class MediaSchemeHandler: NSObject, WKURLSchemeHandler, URLSessionDataDelegate {
         return URLSession(configuration: config, delegate: self, delegateQueue: nil)
     }()
 
-    private var activeTasks: [Int: WKURLSchemeTask] = [:]
+    private var activeTasks: [Int: MediaSchemeTaskState] = [:]
     private var urlSessionTasks: [Int: URLSessionDataTask] = [:]
-    private var originalURLs: [Int: URL] = [:]
     private let lock = NSLock()
 
     func webView(_ webView: WKWebView, start urlSchemeTask: WKURLSchemeTask) {
@@ -51,20 +50,27 @@ class MediaSchemeHandler: NSObject, WKURLSchemeHandler, URLSessionDataDelegate {
 
         let dataTask = session.dataTask(with: realURL)
         lock.lock()
-        activeTasks[dataTask.taskIdentifier] = urlSchemeTask
+        activeTasks[dataTask.taskIdentifier] = MediaSchemeTaskState(
+            schemeTask: urlSchemeTask,
+            originalURL: url
+        )
         urlSessionTasks[dataTask.taskIdentifier] = dataTask
-        originalURLs[dataTask.taskIdentifier] = url
         lock.unlock()
         dataTask.resume()
     }
 
     func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) {
         lock.lock()
-        let taskId = activeTasks.first(where: { $0.value === urlSchemeTask })?.key
+        let taskId = activeTasks.first {
+            $0.value.schemeTask === urlSchemeTask
+        }?.key
+        let schemeTaskState = taskId.flatMap {
+            activeTasks[$0]
+        }
+        schemeTaskState?.stop()
         let dataTask = taskId.flatMap { urlSessionTasks.removeValue(forKey: $0) }
         if let taskId {
             activeTasks.removeValue(forKey: taskId)
-            originalURLs.removeValue(forKey: taskId)
         }
         lock.unlock()
 
@@ -80,8 +86,7 @@ class MediaSchemeHandler: NSObject, WKURLSchemeHandler, URLSessionDataDelegate {
         completionHandler: @escaping (URLSession.ResponseDisposition) -> Void
     ) {
         lock.lock()
-        guard let schemeTask = activeTasks[dataTask.taskIdentifier],
-              let originalURL = originalURLs[dataTask.taskIdentifier] else {
+        guard let schemeTaskState = activeTasks[dataTask.taskIdentifier] else {
             lock.unlock()
             completionHandler(.cancel)
             return
@@ -102,40 +107,115 @@ class MediaSchemeHandler: NSObject, WKURLSchemeHandler, URLSessionDataDelegate {
         }
 
         let schemeResponse = HTTPURLResponse(
-            url: originalURL,
+            url: schemeTaskState.originalURL,
             statusCode: httpResponse.statusCode,
             httpVersion: "HTTP/1.1",
             headerFields: headers
         )!
 
-        schemeTask.didReceive(schemeResponse)
-        completionHandler(.allow)
+        if schemeTaskState.didReceive(schemeResponse) {
+            completionHandler(.allow)
+        } else {
+            completionHandler(.cancel)
+        }
     }
 
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
         lock.lock()
-        guard let schemeTask = activeTasks[dataTask.taskIdentifier] else {
+        guard let schemeTaskState = activeTasks[dataTask.taskIdentifier] else {
             lock.unlock()
             return
         }
         lock.unlock()
-        schemeTask.didReceive(data)
+        schemeTaskState.didReceive(data)
     }
 
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
         lock.lock()
-        let schemeTask = activeTasks.removeValue(forKey: task.taskIdentifier)
+        let schemeTaskState = activeTasks[task.taskIdentifier]
         urlSessionTasks.removeValue(forKey: task.taskIdentifier)
-        originalURLs.removeValue(forKey: task.taskIdentifier)
         lock.unlock()
 
-        guard let schemeTask else { return }
+        guard let schemeTaskState else { return }
 
+        var didComplete = false
         if let error, (error as NSError).code != NSURLErrorCancelled {
-            schemeTask.didFailWithError(error)
+            didComplete = schemeTaskState.didFail(with: error)
         } else if error == nil {
-            schemeTask.didFinish()
+            didComplete = schemeTaskState.didFinish()
         }
+
+        if didComplete {
+            lock.lock()
+            if activeTasks[task.taskIdentifier] === schemeTaskState {
+                activeTasks.removeValue(forKey: task.taskIdentifier)
+            }
+            lock.unlock()
+        }
+    }
+}
+
+private final class MediaSchemeTaskState {
+    let schemeTask: WKURLSchemeTask
+    let originalURL: URL
+
+    private let lock = NSLock()
+    private var isStopped = false
+    private var isCompleted = false
+
+    init(schemeTask: WKURLSchemeTask, originalURL: URL) {
+        self.schemeTask = schemeTask
+        self.originalURL = originalURL
+    }
+
+    func stop() {
+        lock.lock()
+        isStopped = true
+        lock.unlock()
+    }
+
+    @discardableResult
+    func didReceive(_ response: URLResponse) -> Bool {
+        lock.lock()
+        defer { lock.unlock() }
+
+        guard !isStopped, !isCompleted else { return false }
+
+        schemeTask.didReceive(response)
+        return true
+    }
+
+    func didReceive(_ data: Data) {
+        lock.lock()
+        defer { lock.unlock() }
+
+        guard !isStopped, !isCompleted else { return }
+
+        schemeTask.didReceive(data)
+    }
+
+    @discardableResult
+    func didFinish() -> Bool {
+        lock.lock()
+        defer { lock.unlock() }
+
+        guard !isStopped, !isCompleted else { return false }
+
+        isCompleted = true
+        schemeTask.didFinish()
+        return true
+    }
+
+    @discardableResult
+    func didFail(with error: Error) -> Bool {
+        lock.lock()
+        defer { lock.unlock() }
+
+        guard !isStopped, !isCompleted else { return false }
+
+        isCompleted = true
+        schemeTask.didFailWithError(error)
+        return true
     }
 }
 
