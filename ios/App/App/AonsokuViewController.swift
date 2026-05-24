004d615a276c790e90b994f3b6fbd9a770cbdc16
realtvop <realtvop@realtvop.top>
2026-05-24 17:07:20 +0800

    fix(ios): disable text editing gestures in non-input areas

--- ios/App/App/AonsokuViewController.swift
+++ ios/App/App/AonsokuViewController.swift
@@ -5,6 +5,11 @@ import WebKit
 class AonsokuViewController: CAPBridgeViewController {
     private let mediaSchemeHandler = MediaSchemeHandler()
 
+    @available(iOS 16.4, *)
+    override var editingInteractionConfiguration: UIEditingInteractionConfiguration {
+        .none
+    }
+
     override func webViewConfiguration(for instanceConfiguration: InstanceConfiguration) -> WKWebViewConfiguration {
         let config = super.webViewConfiguration(for: instanceConfiguration)
         config.setURLSchemeHandler(mediaSchemeHandler, forURLScheme: "aonsoku-media")
